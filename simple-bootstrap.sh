f7777703dc17707d05e1ed161ef113316560d2f5
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2024-11-04 20:57:43 +0000

    simple-bootstrap: implement existing workaround for upstream issue with salt-bootstrap

--- simple-bootstrap.sh
+++ simple-bootstrap.sh
@@ -8,7 +8,23 @@ SALT_VERSION=${SALT_VERSION:-3006}
 
 ##########
 # Step 1: Install Saltstack and git
-wget -O - https://raw.githubusercontent.com/saltstack/salt-bootstrap/0899e72c34958da284c29fd71eeddb52cc5abe9f/bootstrap-salt.sh | sh -s -- stable ${SALT_VERSION}
+#wget -O - https://bootstrap.saltproject.io | sh -s -- stable ${SALT_VERSION}
+#wget -O - https://raw.githubusercontent.com/saltstack/salt-bootstrap/0899e72c34958da284c29fd71eeddb52cc5abe9f/bootstrap-salt.sh | sh -s -- stable ${SALT_VERSION}
+# workaround for https://github.com/saltstack/salt-bootstrap/issues/2027
+# also see salt_bootstrap.sh -r
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://packages.broadcom.com/artifactory/api/security/keypair/SaltProjectKey/public | tee /etc/apt/keyrings/salt-archive-keyring.pgp
+curl -fsSL https://github.com/saltstack/salt-install-guide/releases/latest/download/salt.sources | tee /etc/apt/sources.list.d/salt.sources
+
+echo "Package: salt-*
+Pin: version ${SALT_VERSION}.*
+Pin-Priority: 1001" | tee /etc/apt/preferences.d/salt-pin-1001
+
+# grab installer script
+wget -L https://github.com/saltstack/salt-bootstrap/releases/latest/download/bootstrap-salt.sh --output-document salt_bootstrap.sh --no-verbose
+
+sh salt_bootstrap.sh -r -F -c /tmp onedir "${SALT_VERSION}"
+
 # disable the service until configured
 service salt-minion stop
 # the bootstrap formula might need git installed..
